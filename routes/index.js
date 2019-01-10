3e9da72fcef37671318e00dffbdd68b7d1fd1331
Mason <me@masonx.ca>
2019-01-09 20:23:29 -0500

    Add simple tweak to simplify interfaces
    A JSON string with all the songs will make Stretto easier to interface with (e.g. mopidy)

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ exports.createRoutes = function(app_ref) {
   app.get('/', musicRoute);
   app.get('/remote/:name', musicRoute);
   app.get('/songs/:id.mid', sendSong);
+  app.get('/songs', function(req, res){get_songs(function(songs){res.json(songs);res.end();});});
   app.get('/cover/:id', sendCover);
   app.get('/downloadplaylist/:id', downloadPlaylist);
   app.post('/upload', uploadSong);
